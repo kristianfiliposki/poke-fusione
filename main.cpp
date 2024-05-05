044f5df75c840b8f200930746500a0ddf8a4753f
kristianfiliposki <krisplay@outlook.it>
2024-05-06 00:10:02 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int persona = 12;
+    persona=13;
+
+    std::cout <<"ciao a tutte le" <<std::endl;
+    std::cout << persona << std::endl;
+    std::cout <<"persone" <<std::endl;
+
+    return 0;
+
+}
